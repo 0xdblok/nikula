5d4c05b2dfa16946f44f5d9511042ebc0b540b53
0xdblok <dblok0x@gmail.com>
2024-04-20 00:00:19 +0200

    ee

--- app/component/tokenomics.js
+++ app/component/tokenomics.js
@@ -9,20 +9,65 @@ export default function Tokenomics() {
         </h2>
         <div className="grid gap-6 my-16 lg:grid-cols-3">
           <div className="flex flex-col p-8 space-y-4 rounded-md dark:bg-gray-50">
-            <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full dark:bg-violet-600 dark:text-gray-50">
-              1
+            <div className="flex">
+              <div className="flex-shrink-0">
+                <div className="flex items-center justify-center w-12 h-12 rounded-md bg-violet-600 text-gray-50">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    className="w-7 h-7">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M5 13l4 4L19 7"></path>
+                  </svg>
+                </div>
+              </div>
             </div>
             <p className="text-2xl font-semibold">TOTAL SUPPLY : 1B</p>
           </div>
           <div className="flex flex-col p-8 space-y-4 rounded-md dark:bg-gray-50">
-            <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full dark:bg-violet-600 dark:text-gray-50">
-              2
+            <div className="flex">
+              <div className="flex-shrink-0">
+                <div className="flex items-center justify-center w-12 h-12 rounded-md bg-violet-600 text-gray-50">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    className="w-7 h-7">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M5 13l4 4L19 7"></path>
+                  </svg>
+                </div>
+              </div>
             </div>
             <p className="text-2xl font-semibold">100% LP</p>
           </div>
           <div className="flex flex-col p-8 space-y-4 rounded-md dark:bg-gray-50">
-            <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full dark:bg-violet-600 dark:text-gray-50">
-              3
+            <div className="flex">
+              <div className="flex-shrink-0">
+                <div className="flex items-center justify-center w-12 h-12 rounded-md bg-violet-600 text-gray-50">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    className="w-7 h-7">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M5 13l4 4L19 7"></path>
+                  </svg>
+                </div>
+              </div>
             </div>
             <p className="text-2xl font-semibold">NO PRESALE</p>
             <p className="text-2xl font-semibold"> LP LOCKED</p>
